7548ee0eeb5b98d15d65a9ab51aa6999af013b4b
gurdeep2 <gurdeepkalsia@gmail.com>
2018-06-13 23:32:34 -0700

    assignment1

---
+++ assignmnt1.py
@@ -0,0 +1,66 @@
+# 1 create the list with user defined input 
+x=input("enter the name")
+y=input("enter the name")
+z=input("enter the name")
+w=input("enter the name")
+list=[x,y,z,w]
+print(list)
+
+
+
+#add the following list to the above created list
+#("google","apple","facebook","microsoft","tesla")
+list2=["google","apple","facebook","microsoft","tesla"]
+print(list.extend(list2))
+print(list)
+
+
+
+# 3 create the list with number and sort it in ascending order
+list=[3,5,8,2,9]
+print(list.sort()
+print (list)
+
+
+
+#4 wap to merge them into a single sorted array in ascending order
+A=[3,4,5,6]
+print(A.sort())
+print(A)
+B=[3,4,5,6]
+print(B.sort())
+print(B)
+C=(A+B)
+print(C.sort())
+print(c)
+
+
+
+
+
+#5 count the number of time object occur.
+list[2,7,3,4,7,5,6,7,8]
+print(list.count(7))
+
+
+
+
+#6count even and odd in a list.
+n=int(input("enter a number:"))
+if n%2==0:
+   print("n is even")
+else:
+   print("n is odd")
+
+
+#7 implement stack and queue using list.
+#for stack
+list=[1,2,3,4]
+print (list.pop())
+#for queue
+list=[1,2,3,4]
+print(list)
+list.reverse()
+print(list)
+print(list.pop())
+   
