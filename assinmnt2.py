8b60590dcec197e46a472e168e6bc2edff52bb2e
gurdeep2 <gurdeepkalsia@gmail.com>
2018-06-14 00:04:00 -0700

    assinmnt2

---
+++ assinmnt2.py
@@ -0,0 +1,34 @@
+# print anything you want on screen.
+print("i hate you")
+
+
+# join two strings '+'
+x="aish"
+y="deep"
+print(x+y)
+
+
+
+#take the input of 3 variable x,y,z. print their values on screen
+x=int(input("enter the value:")
+y=int(input("enter the value:")
+z=(x+y)
+print(z)
+
+
+# print ("let's get started") on screen
+print('let\'s get started')
+
+
+
+# print placeholders 
+s="acadview"
+course="python"
+fees =int(5000)
+print("%s"%s"%d")%(s,course,fees)
+
+
+# exercise
+name=("tonny strak")
+salary =int(100000)
+print("%s %d"%(name,salary))
