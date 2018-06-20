8cea0a0228877b23ac2ba14515d3c283371beb57
gurdeep2 <gurdeepkalsia@gmail.com>
2018-06-19 23:39:37 -0700

    assinmnt8 add

--- assinmnt8.py
+++ assinmnt8.py
@@ -14,7 +14,9 @@
 # 6	Day of Week	0 to 6 (0 is Monday)
 # 7	Day of year	1 to 366 (Julian day)
 # 8	Daylight savings	-1, 0, 1, -1 means library determines DST
-# The above tuple is equivalent to struct_time structure. T
+# The above tuple is equivalent to struct_time structure. 
+import time
+print(time.gmtime())
 
 
 #2.write a program to get formatted time.
@@ -24,5 +26,46 @@ print(time.asctime(time.localtime(time.time())))
  
  
    
-#3.extract month from he time
-#4.exract day from the time   
+#3.extract month from the time
+#extracting month
+import datetime 
+print(datetime.datetime.now().strftime('%m'))
+
+
+#4.extract day from the time.
+import datetime 
+print(datetime.datetime.now().strftime('%a'))
+
+#5.extract date from the time.
+import datetime
+s=datetime.date.today()
+print(s.day)
+
+#6.write a program to print time using localtime method.
+import time
+print(time.localtime())
+
+
+
+
+#7 find the factorial of a number input by user using math package function.
+import math
+print(math.factorial(int(input("enter any number:"))))
+
+
+
+#8 FIND THE GCD OF A NUMBER INPUT BY USER USING MATH PACKAGE FUNCTION
+x=int(input("enter number"))
+y=int(input("enter other number"))
+import math
+print(math.gcd(x,y ))
+
+
+#9.use os package and do thefollowing
+#get current working directory
+import os
+print(os.getcwd)
+
+#get the user environment.
+import os
+print(os.environ)   
